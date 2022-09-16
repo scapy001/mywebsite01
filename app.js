d7ab8e0a7284b60dd55f4bf7ec8f0348b5e49d9f
scapy001 <113714308+scapy001@users.noreply.github.com>
2022-09-16 23:40:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,386 @@
+//Question bank
+var questionBank= [ 
+    {
+        question : 'Bio gas Contains',
+        option : ['CO₂','H2','CH₄','all of these'],
+        answer : 'CH₄'
+    },
+    {
+        question : 'IN WHICH STAGE DOES CROSSING-OVER TAKE PLACE ?',
+        option : ['LEPTOTENE', 'CYTOKINESIS','PACHYTENE', 'DIAKINESIS'],
+        answer : 'CYTOKINESIS'
+    },
+    {
+        question : 'PCR IS USED TO DETECT',
+        option : ['HIV','T.B.','CHOLERA','CANCER'],
+        answer : 'HIV'
+    },
+    {
+        question : 'EVERY LIVING CELL OF PLANT CAN GIVE RISE TO THE WHOLE PLANT. THIS PROPERTY IS KNOWN AS ',
+        option : ['CLONING','SOMACLONAL','TOTIPOTENCY','ALL OF THESE'],
+        answer : 'TOTIPOTENCY'
+    },
+    {
+        question : 'WHAT IS THE PLOIDY LEVEL OF CHROMOSOMES IN ENDOSPERM OF ANGIOSPERMIC PLANT ?',
+        option : ['N','3N','2N','BOTH A AND C '],
+        answer : '3N'
+    },
+    {
+        question : 'WHEN OFFSPRING IS FORMED BY SINGLE PARENT, IT IS CALLED AS',
+        option : ['INTERNAL FERTILIZATION ','ASEXUAL REPRODUCTION','BOTH (A) AND (B) ','SEXUAL REPRODUCTION '],
+        answer : 'ASEXUAL REPRODUCTION '
+    },
+    {
+        question : 'WHAT IS THE NUMBER OF CHROMOSOMES PRESENT IN HUMAN GAMETES ?',
+        option : ['21','23','44','46'],
+        answer : '23'
+    },
+    {
+        question : 'WHICH OF THE FOLLOWING IS NOT A VIRAL DESEASE',
+        option : ['MUMPS','INFLUENZA ','DIPHTHERIA','MEASLES'],
+        answer : 'INFLUENZA '
+    },
+    {
+        question : 'WHICH OF THE FOLLOWING IS NOT INCLUDED IN ANIMAL HUSBANDRY',
+        option : ['ORGANIC FARMING ','FISH FARMING ','POULTRY FARMING ','BEE-KEEPING'],
+        answer : 'ORGANIC FARMING '
+    },
+    {
+        question : 'WHO AMONG THE FOLLOWING IS NOT RELATED WITH MICROBIOLOGY ?',
+        option : ['LOUIS PASTEUR','J.D. WATSON','STEPHAN HALES ','ROBERT KOCH '],
+        answer : 'STEPHAN HALES '
+    },
+    {
+        question : 'ANTICODONS ARE FOUND IN ',
+        option : ['m-RNA ','t-RNA','r-RNA','NONE OF THESE'],
+        answer : 't-rna'
+    },
+    {
+        question : 'MENDEL PROPOSED ',
+        option : ['LAW OF LINKAGE','LAW OF INHERITANCE','BOTH (A) AND (B)','NONE OF THESE '],
+        answer : 'LAW OF INHERITANCE '
+    },
+    {
+        question : 'LAW OF SEGREGRATION IS ALSO KNOWN AS ',
+        option : ['LAW OF DOMINANCE','LAW OF INDEPENDENT ASSORTMENT','LAW OF PURITY OF GAMETES','NONE OF THESE'],
+        answer : 'LAW OF PURITY OF GAMETES '
+    },
+    {
+        question : 'WHICH OF THE FOLLOWING FACTORS IS IMPORTANT IN THE FORMATION OF NEW SPECIES ?',
+        option : ['COMPETITION','MUTATION','ISOLATION','CONTINUOUS VARIATION '],
+        answer : 'MUTATION'
+    },
+    {
+        question : 'WHICH OT THE FOLLOWING IS / ARE INSECTIVOROUS PLANT(S) ?',
+        option : ['DROSERA','NEPENTHES','BOTH (A) AND (B)','HYDRILLA'],
+        answer : 'NEPENTHES'
+    },
+    {
+        question : 'OUT OF THE FOLLOWING WHICH ONE IS HERMAPHRODITE ORGANISM ?',
+        option : ['HEN','SNAKE','BOTH (A) AND (B)','EARTHWORM'],
+        answer : 'EARTHWORM'
+    },
+    {
+        question : 'CHLORELLA IS A TYPE OF ',
+        option : ['ALGAE','BACTERIA','PROTOZOA','SINGLE CELL PROTEIN'],
+        answer : 'ALGAE'
+    },
+    {
+        question : 'CRY-GENE PREVENTS WHICH CROP FROM BOLLWORM',
+        option : ['COTTON','TEA','MANGO','WHEAT'],
+        answer : 'COTTON'
+    },
+    {
+        question : 'PURINE BASES OF DNA ARE ',
+        option : ['ADENINE AND CYTOSINE','CYTOSINE AND THYMINE','ADENINE AND GUANINE','NONE OF THESE'],
+        answer : 'ADENINE AND GUANINE'
+    },
+    {
+        question : 'CANCER IS CAUSED BY ',
+        option : ['BACTERIA','ONCOGENES','BOTH (A) AND (B)','NONE OF THESE'],
+        answer : 'NONE OF THESE '
+    },
+    {
+        question : 'WHICH ONE OF THE FOLLOWING IS POSSIBLE IN MICROPROPAGATION ?',
+        option : ['ASEXUAL REPRODUCTION','SEXUAL REPRODUCTION','BOTH (A) AND (B)','NONE OF THESE'],
+        answer : 'ASEXUAL REPRODUCTION'
+    },
+    {
+        question : 'RESTRICTION ENZYMES ARE',
+        option : ['EXONUCLEASE','ENDONUCLEASE','LIGASE','POLYMERASE'],
+        answer : 'ENDONUCLEASE'
+    },
+    {
+        question : 'IN NATIONAL PARK, PROTECTION IS PROVIDED TO',
+        option : ['FLORA','FAUNA','ECOSYSTEM','BOTH (A) AND (B)'],
+        answer : 'ECOSYSTEM'
+    },
+    {
+        question : 'PASTEURIZATION IS HEATING OF ',
+        option : ['MILK ONLY','ANY LIQUIDS ABOVE 100 DEGREE','ANY LIQUIDS AT 70 DEGREE ','NONE OF THESE '],
+        answer : ''
+    },
+    {
+        question : 'THE WALL OF POLLEN GRAINS IS ',
+        option : ['SINGLE LAYERED','DOUBLE LAYERED','TRIPLE LAYERED','MULTI-LAYERED'],
+        answer : 'TWO LAYEDRED'
+    },
+    {
+        question : 'T-LYMPHOCYTE ORIGINATES FROM ',
+        option : ['BONE MARROW','E.COLI','BOTH (A) AND (B)','HIND111'],
+        answer : 'BONE MARROW'
+    },
+    {
+        question : 'SOURCE OF ECORI ENZYME IS',
+        option : ['BAM H1','E.COLI','BOTH (A) AND (B)','HIND111'],
+        answer : 'E.COLI'
+    },
+    {
+        question : 'ECHIDNA IS',
+        option : ['CONNECTING LINK','VESTIGIAL ORGAN','EXTINCT LINK','NONE OF THESE'],
+        answer : 'CONNECTING LINK'
+    },
+    {
+        question : 'DODO IS ',
+        option : ['EXTINCT SPECIES','ENDANGERED SPECIES','THREATENED SPECIES','NONE OF THESE'],
+        answer : 'EXTINCT SPECIES'
+    },
+    {
+        question : 'WHICH OF THE FOLLOWING IS A DIPLOID STRUCTURE ?',
+        option : ['OVUM','ENDOSPERM','ZYGOTE','ALL OF THESE'],
+        answer : 'ZYGOTE'
+    },
+    {
+        question : 'WHICH ONE IS HYDROPHYTIC PLANT ?',
+        option : ['TRAPA','OPUNTIA','DALBERGIA','ACACIA'],
+        answer : 'TRAPA'
+    },
+    {
+        question : 'SETOLI CELLS ARE FOUND IN',
+        option : ['TESTIS','UTERUS','OVARY','LIVER'],
+        answer : 'TESTIS'
+    },
+    {
+        question : 'RHINO SANCTUARY IS LOCATED IN WHICH STATE ?',
+        option : ['ASSAM','WEST BENGAL','UTTAR PRADESH','BIHAR'],
+        answer : 'ASSAM'
+    },
+    {
+        question : 'KANHA NATIONAL PARK IS FAMOUS FOR',
+        option : ['BIRDS','TIGERS','RHINOCEROS','CROCODILES'],
+        answer : 'TIGER'
+    },
+    {
+        question : 'THE PROCESS OF FORMATION OF GAMETES IS CALLED',
+        option : ['GAMETOGENESIS','CYTOKINESIS','SPOROGENESIS','NONE OF THESE'],
+        answer : 'GAMETOGENESIS'
+    },
+    {
+        question : 'AN EXCEPTION OF MENDELS LAW IS',
+        option : ['DOMINANCE','PURITY OF GAMETE','LINKAGE','INDEPENDENT ASSORTMENT'],
+        answer : 'LINKAGE'
+    },
+    {
+        question : 'AGARROSE IS EXTRACTED FROM',
+        option : ['MAIZE','SEA WORDS','CYCAS','NONE OF THESE'],
+        answer : 'SEA WORDS'
+    },
+    {
+        question : 'OPERON MODEL WAS PROPOSED BY',
+        option : ['WATSON AND CRICK','NIRENBERG','JACOB AND MONAD','NONE OF THESE'],
+        answer : 'JACOB AND MONOD'
+    },
+    {
+        question : 'WIDAL TEST IS DONE TO CONFIRM',
+        option : ['MALARIA','TYPHOID','AIDS','CANCER'],
+        answer : 'TYPHOID'
+    },
+    {
+        question : 'HIV ATTACKS ON WHICH OF THE FOLLOWING CELLS?',
+        option : ['B-CELLS','T-CELLS','EPITHELIAL CELL','T-HELPER CELL'],
+        answer : 'T-HELPER CELLS'
+    },
+    {
+        question : 'CHEMICAL RELASED DUE TO ALLERGY IS',
+        option : ['HISTAMINE','SEROTONIN','BOTH(A) AND (B)','NONE OF THESE'],
+        answer : 'HISTAMINE'
+    },
+    {
+        question : ' Which one of the following is not an ore of zinc ?',
+        option : ['Zinc blende','Calamine',' Zincite ',' Bauxite'],
+        answer : ' Bauxite'
+    },
+    {
+        question : ' Which of the following metals does not liberate hydrogen gas on reactions with acid ?',
+        option : ['CU','FE','ZN','MG'],
+        answer : 'CU'
+    },
+    {
+        question : 'Mineral from which metal is extracted, is called',
+        option : ['ore',' Gangue','slag','none of these'],
+        answer : 'ore'
+    },
+    {
+        question : 'Which of the following oxides is reduced in blast furnace ? ',
+        option : ['oxide of iron ','oxide of copper','sodium oxide ','zinc oxide'],
+        answer : 'oxide of iron'
+    },
+    {
+        question : 'When a metal oxide is heated with carbon, then metal is formed, MxOy + yC → XM + yCO, here carbon acts as',
+        option : ['oxidising agent',' Reducing agent',' Stabilizer ','None of These'],
+        answer : 'oxidising agent'
+    },
+    {
+        question : 'The molality of pure water is',
+        option : ['55.5','50.5','18','60.5'],
+        answer : '55.5'
+    },
+    {
+        question : 'The number of moles of NaCl in 3 litres of 3M solution is',
+        option : ['1','3','9','27'],
+        answer : '9'
+    },
+    {
+        question : '4L of 0.02 M aqueous solution of NaCl was diluted by adding one litre of water. The molality of the resultant solution is ',
+        option : ['0.004','0.008','0.012','0.016'],
+        answer : '0.016'
+    },
+    {
+        question : 'What is mole fraction of solute in 1.00 m aqueous solution?',
+        option : ['0.0354','0.0177','0.177','1.770'],
+        answer : '0.0177'
+    },
+    {
+        question : ' How many grams of NaOH are present in 250 mL of 0.5 M NaOH solution ?',
+        option : ['32 g',' 3.8 g',' 5 g','0.5 g'],
+        answer : ' 5 g'
+    },
+    {
+        question : 'Which of the following aqueous solutions should have the highest boiling point?',
+        option : ['1.0 M NaOH',' 1.0 M Na2SO4','1.0 M NH4NO3','1.0 M KNO3'],
+        answer : ' 1.0 M Na2SO4'
+    },
+    {
+        question : 'What is the effect of Frenkel defect on the density of ionic solids?',
+        option : ['The density of the crystal increases','The density of the crystal decrease','The density of the crystal remains unchanged','There is no relationship between density of a crystal and defect present in it'],
+        
+        answer : 'The density of the crystal remains unchanged'
+    },
+    {
+        question : ' The impurity doped in germanium to obtain n-type germanium is',
+        option : [' Tetravalent ','Trivalent','Pentavalent ','None of these'],
+        answer : 'Pentavalent '
+    },
+    {
+        question : 'In full wave rectification, if input frequency is 50 Hz, then output frequency will be',
+        option : ['50 Hz','25 Hz','100 Hz','200 Hz '],
+        answer : ' 100 Hz'
+    },
+    {
+        question : 'Main gate (Basic gate) is ',
+        option : [' AND, OR ',' NAND, NOR',' OR, NOT','AND, OR, NOT '],
+        answer : ' AND, OR, NOT '
+    },
+    {
+        question : 'For Binary number 10101, the number in decimal representation will be',
+        option : ['31','11','21','03'],
+        answer : '11'
+    },
+    {
+        question : ' With the increases of temperature the resistivity of semiconductor',
+        option : ['increases ','decreases','remains constant','becomes zero'],
+        answer : 'decreases'
+    },
+
+    {
+        question : 'To make a P-type semiconductor Germanium is doped with',
+        option : ['Gallium ','Boron','Aluminium ',' Phosphorus'],
+        answer : ' Gallium '
+    },
+    {
+        question : 'Which one of the following is the unit of electric field ',
+        option : ['Coulomb ','Newton','Volt ',' N/C'],
+        answer : '  N/C '
+    },
+
+
+
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
